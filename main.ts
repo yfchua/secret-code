fc78e97a87780e8c8b1175438202bdba37c18b81
yfchua <35942337+yfchua@users.noreply.github.com>
2021-07-04 15:30:42 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index <= 5; index++) {
+        x = Math.randomBoolean()
+        if (x == true) {
+            l = "A"
+        } else {
+            l = "B"
+        }
+    }
+})
+let x = false
+let l = ""
+l = ""
+x = true
+let secretcode = ""
+basic.forever(function () {
+	
+})
